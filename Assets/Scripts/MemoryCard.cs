c02b82e23a5b19dd79b0746582c406ccd42012d7
Sergey Beketov <sergey.beketov@bsc-ideas.com>
2017-01-20 20:06:05 +0300

    add unreveal card

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject _cardBack;
+    [SerializeField] private SceneController _sceneController;
+
+    public int Id { get; private set; }
+
+    // Use this for initialization
+    private void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+    private void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (!_cardBack.activeSelf || !_sceneController.CanReveal) return;
+        _cardBack.SetActive(false);
+        _sceneController.CardRevealed(this);
+    }
+
+    public void Unreveal()
+    {
+        _cardBack.SetActive(true);
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        Id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public const int GridRows = 2;
+    public const int GridCols = 4;
+    public const float OffsetX = 2f;
+    public const float OffsetY = 2.5f;
+
+    [SerializeField] private MemoryCard _originalCard;
+    [SerializeField] private Sprite[] _images;
+
+    private MemoryCard _firstRevealed;
+    private MemoryCard _secondRevealed;
+    private int _score = 0;
+
+    public bool CanReveal
+    {
+        get { return _secondRevealed == null; }
+    }
+
+    // Use this for initialization
+    private void Start ()
+    {
+        var startPos = _originalCard.transform.position;
+
+        int[] numbers = {0, 0, 1, 1, 2, 2, 3, 3};
+
+        numbers = ShuffleArray(numbers);
+
+        for (var i = 0; i < GridCols; i++)
+        {
+            for (var j = 0; j < GridRows; j++)
+            {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                {
+                    card = _originalCard;
+                }
+                else
+                {
+                    card = Instantiate(_originalCard);
+                }
+
+                var index = j * GridCols + i;
+                var id = numbers[index];
+                card.SetCard(id, _images[id]);
+
+                var posX = OffsetX * i + startPos.x;
+                var posY = -(OffsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+    private void Update () {
+		
+	}
+
+    public void CardRevealed(MemoryCard card)
+    {
+        if (_firstRevealed == null)
+        {
+            _firstRevealed = card;
+        }
+        else
+        {
+            _secondRevealed = card;
+            StartCoroutine(CheckMatch());
+        }
+    }
+
+    private static int[] ShuffleArray(int[] numbers)
+    {
+        var newArray = numbers.Clone() as int[];
+        Debug.Assert(newArray != null, "newArray != null");
+        for (var i = 0; i < newArray.Length; i++)
+        {
+            var tmp = newArray[i];
+            var r = Random.Range(i, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = tmp;
+        }
+        return newArray;
+    }
+
+    private IEnumerator CheckMatch()
+    {
+        if (_firstRevealed.Id == _secondRevealed.Id)
+        {
+            _score++;
+            Debug.Log("Score: " + _score);
+        }
+        else
+        {
+            yield return new WaitForSeconds(.5f);
+            _firstRevealed.Unreveal();
+            _secondRevealed.Unreveal();
+        }
+
+        _firstRevealed = null;
+        _secondRevealed = null;
+    }
+}
